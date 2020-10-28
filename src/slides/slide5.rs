3349c2801dc9d969ab38e4c0bb2e0e13553fa31a
Raik Joachim <raik.joachim@cosee.biz>
2020-10-28 16:02:29 +0100

    rework slide5 a bit to include some feedback

--- src/slides/slide5.rs
+++ src/slides/slide5.rs
@@ -15,60 +15,71 @@ mod definitions {
     }
 }
 
-fn get_from_predefined_map(key: &String) -> Option<String> {
+// Helfermethode
+fn get_from_predefined_map(key: String) -> Option<String> {
     use std::collections::HashMap;
     let mut map = HashMap::new();
     map.insert("Schuld und Sühne".to_string(), "Dostojewski".to_string());
     map.insert("Die Verwandlung".to_string(), "Kafka".to_string());
     map.insert("Der Schneesturm".to_string(), "Sorokin".to_string());
 
-    match map.get(key) {
+    match map.get(&key) {
         Some(value) => Some(value.clone()),
         None => None,
     }
 }
 
-#[cfg(test)]
-#[test]
-fn test_map_showcase_unwrap() {
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
-    let mut author_match = "Unknown".to_string();
-
-    // Geht nicht. Wir bekommen ein Option zurück, kein String.
-    // Man den Fall, dass kein Element gefunden wurde handhaben.
-    //   author = item;
-
+fn ok_or_abort(maybe_value: Option<String>) -> String {
     // Hole das Objekt aus der Some Variante, ansonsten beende
     // das Programm.
-    author_match = match item {
+    let ret = match maybe_value {
         Some(a) => a,
         None => panic!("Tried to assign None"),
     };
 
+    ret
+}
+
+#[cfg(test)]
+#[test]
+fn test_showcase_unwrap() {
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
+    let mut author_match = "Unknown".to_string();
+
+    // Geht nicht. Wir bekommen ein Option zurück, kein String.
+    // Man den Fall, dass kein Element gefunden wurde handhaben.
+    //   author_match = item;
+
+    author_match = ok_or_abort(item);
+
     // äquivalent: unwrap
 
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
     let mut author_unwrap = "Unknown".to_string();
 
     author_unwrap = item.unwrap();
 }
 
-#[cfg(test)]
-#[test]
-fn test_map_showcase_unwrap_or() {
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
-    let mut author_match = "Unknown".to_string();
-
+fn ok_or_default(maybe_value: Option<String>, default: String) -> String {
     // Hole das Objekt aus der Some Variante, ansonsten befülle
     // die Variable mit einem default Wert.
-    author_match = match item {
-        Some(a) => a,
-        None => "Unbekannt".to_string(),
+    let ret = match maybe_value {
+        Some(s) => s,
+        None => default,
     };
+
+    ret
+}
+
+#[cfg(test)]
+#[test]
+fn test_showcase_unwrap_or() {
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
+    let author_match = ok_or_default(item, "Unknown".to_string());
 
     // äquivalent: unwrap_or
 
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
     let mut author_unwrap = "Unknown".to_string();
 
     author_unwrap = item.unwrap_or("Unbekannt".to_string());
@@ -77,22 +88,26 @@ fn test_map_showcase_unwrap_or() {
 }
 
 pub fn try_operator() -> Option<String> {
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
     let mut author_match = "Unknown".to_string();
 
     // Hole das Objekt aus der Some Variante, ansonsten gib None
-    // an unseren Aufrufer zurück.
+    // an unseren Aufrufer zurück (early out).
     author_match = match item {
         Some(a) => a,
         None => return None,
-    };
+    }
+    // Wenn wir hier ankommen haben wir garantiert einen String.
+    .to_uppercase();
 
     // äquivalent: Der try-operator
 
-    let item = get_from_predefined_map(&"Die Verwandlung".to_string());
+    let item = get_from_predefined_map("Die Verwandlung".to_string());
     let mut author_try = "Unknown".to_string();
 
-    author_try = item?;
+    author_try = item?.to_uppercase();
+
+    assert_eq!(author_match, author_try);
 
     if author_try.len() > 12 {
         Some("Der Author hat einen langen Namen".to_string())
@@ -101,23 +116,39 @@ pub fn try_operator() -> Option<String> {
     }
 }
 
-fn get_content_length(filename: &impl ToString) -> std::io::Result<usize> {
+fn get_first_word_from_file(filename: &impl ToString) -> std::io::Result<String> {
     // try-operator (=?) kann gechained werden
     use std::fs::File;
     use std::io::Read;
     let mut content = String::new();
-    let _ = File::open(filename.to_string())?.read_to_string(&mut content)?;
 
-    Ok(content.len())
+    // Versuche die Datei zu öffenen, wenn erfolgreich versuche ihren
+    // Inhalt in content zu lesen. read_to_string gibt die Anzahl an
+    // gelesenen Bytes zurück. Diese verwerfen wir hier (_).
+    // Sollte open oder read_to_string fehlschlagen, würde diese Funktion
+    // an der Stelle early returnen, und den Fehler zurückgeben.
+    let _ = File::open(filename.to_string())?.read_to_string(&mut content)?;
+    let mut words: Vec<String> = content
+        .split_ascii_whitespace()
+        .map(|s| s.to_string())
+        .collect();
+    let first_word = words.remove(0);
+
+    Ok(first_word)
 }
 
 #[cfg(test)]
 #[test]
 fn test_get_content_length() {
     let filename = "filename.txt";
-    let result = get_content_length(&filename);
+    let result = get_first_word_from_file(&filename);
+    // get_first_word_from_file könnte einen Fehler zurückgegeben haben.
+    // Darauf überprüfen wir hier.
     match result {
-        Ok(s) => println!("We read {} characters from the \"{}\"", s, filename),
+        Ok(s) => {
+            println!("We read the file \"{}\". It says \"{}\"", filename, s);
+            assert_eq!("a", s);
+        },
         Err(e) => println!("The file \"{}\"could not be read!", filename),
     }
 }
